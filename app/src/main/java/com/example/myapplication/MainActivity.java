7ccb984a1e6570045b50c99344cae2b12de32d60
D1NYAY <otorbaev.bro26021@gmail.com>
2022-09-01 13:40:19 +0600

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,149 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    private TextView textView;
+    private Integer first, second;
+    private Boolean isOperationClick;
+    private String operator;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        textView = findViewById(R.id.textView);
+    }
+
+    public void onNumberClick(View view) {
+        switch (view.getId()) {
+            case R.id.btn_One:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("1");
+                }else if (isOperationClick){
+                    textView.setText("1");
+                }
+                else {
+                    textView.append("1");
+                }
+                break;
+            case R.id.btn_two:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("2");
+                    }else if (isOperationClick){
+                        textView.setText("2");
+                } else {
+                textView.append("2");}
+                break;
+            case R.id.btn_three:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("3");
+                }else if (isOperationClick) {
+                    textView.setText("3");
+                }else {
+                textView.append("3");}
+                break;
+            case R.id.btn_four:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("4");
+                }else if (isOperationClick) {
+                    textView.setText("4");
+                }else {
+                textView.append("4");}
+                break;
+            case R.id.btn_five:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("5");
+                }else if (isOperationClick) {
+                    textView.setText("5");
+                } else {
+                textView.append("5");}
+                break;
+            case R.id.btn_six:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("6");
+                }else if (isOperationClick) {
+                    textView.setText("6");
+                } else {
+                textView.append("6");}
+                break;
+            case R.id.btn_seven:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("7");
+                }else if (isOperationClick) {
+                    textView.setText("7");
+                } else {
+                textView.append("7");}
+                break;
+            case R.id.btn_eight:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("8");
+                }else if (isOperationClick) {
+                    textView.setText("8");
+                } else {
+                textView.append("8");}
+                break;
+            case R.id.btn_Nine:
+                if (textView.getText().toString().equals("0")) {
+                    textView.setText("9");
+                }else if (isOperationClick) {
+                    textView.setText("9");
+                } else {
+                textView.append("9");}
+                break;
+            case R.id.btn_zero:
+                textView.append("0");
+                break;
+            case R.id.AC:
+                textView.setText("0");
+                break;
+
+        }
+        isOperationClick = false;
+    }
+
+    public void onOperationClick(View view) {
+        switch (view.getId()) {
+            case R.id.plus:
+                first = Integer.parseInt(textView.getText().toString());
+                break;
+            case R.id.equals:
+                second = Integer.parseInt(textView.getText().toString());
+                Integer result = first + second;
+               textView.setText(result.toString());
+            break;
+            case R.id.btn_minus:
+                first = Integer.parseInt(textView.getText().toString());
+                isOperationClick = true;
+                operator = "-";
+                break;
+            case R.id.division:
+                first = Integer.parseInt(textView.getText().toString());
+                isOperationClick = true;
+                operator = "/";
+                break;
+            case R.id.multiply:
+                first = Integer.parseInt(textView.getText().toString());
+                isOperationClick = true;
+                operator = "*";
+                break;
+
+        }
+        isOperationClick = true;
+    }
+}
+
+
+
+
+
+
+
+
